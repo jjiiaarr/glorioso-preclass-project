1850dfcd597f937decc72ff1f0083db4aea7ec70
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-03-01 17:04:45 +0800

    activity 2 : create a script to make hash with keys first,middle and last names and create function that takes the given hash and returns the fullname

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,14 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+def hashfullname(full_name)
+  name_str = ''
+  # Go through each names:
+  full_name.each do |type,name|
+    name_str = name_str + name + ' '
+  end
+  return name_str
+end
+
+p hashfullname({first_name: 'alfonso', middle_name: 'dumadag', last_name:'glorioso'})
