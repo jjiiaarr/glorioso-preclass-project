e3135e0dbc8aaaee1ac9211b58fb65ecd32aad4e
jjiiaarr <jiiarr.glorioso@gmail.com>
2023-03-13 14:46:06 +0800

    activity 3: create function that takes an array of ages and return with differences between 1st and 2nd age, 2nd age and 3rd.

--- activities/activity_3.rb
+++ activities/activity_3.rb
@@ -2,3 +2,23 @@
 #  Write a function that takes an array of ages.
 #  The function will return an array of age differences between the 1st and 2nd age, 2nd and 3rd  .. so on and so forth.
 #  Print on console the result.
+
+def diffofarray(ages)
+  # Create new array that will store the adult ages
+  diff = []
+
+
+  # Go through each ages values:
+  ages.each_with_index do |str,i|
+    # Check if age is greater than or equal 18:
+    if i <  ages.length - 1
+      # Store age in adults array
+      diff << ages[i] - ages[i+1]
+    end
+    end
+  return diff
+end
+
+
+
+p diffofarray([19, 24, 17, 18, 10, 32])
